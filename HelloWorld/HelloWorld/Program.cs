869014bf8cb68222e7c0faa76e639866964f955e
CristianBanan <cristian.ursache96@yahoo.com>
2017-07-10 13:25:53 +0300

    schimbare

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,6 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Deus Vult!");
             System.Console.Out.Write("Hellow World");
         }
     }
